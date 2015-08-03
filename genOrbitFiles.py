e081d69da23ec5e01ed223d3d9d1e880e52d15f6
Peter Yoachim <yoachim@uw.edu>
2015-08-03 15:19:22 -0700

    update plots a bit

--- genOrbitFiles.py
+++ genOrbitFiles.py
@@ -10,17 +10,20 @@ import os, argparse
 #runNames = ['ops2_1094', 'enigma_1257',
 #            'enigma_1258','enigma_1259','enigma_1189']
 
-def runThem(runName):
-    useCamera=True
+def runThem(runName,useCamera=True):
+
 
     orbitfile = 'pha20141031.des'
-
 
     dbcols = ['expMJD', 'night', 'fieldRA', 'fieldDec', 'rotSkyPos', 'filter',
               'finSeeing', 'fiveSigmaDepth', 'visitExpTime', 'solarElong']
 
     print 'Generating orbit file for %s' % runName
-    outfilename = runName+'_out.txt'
+    extraS = ''
+    if not useCamera:
+        extraS = '_NoCam'
+
+    outfilename = runName+extraS+'_out.txt'
     runMoObs(orbitfile, outfilename,
              '/Users/yoachim/Scratch/Opsim_sqlites/'+runName+'_sqlite.db',
              dbcols=dbcols, useCamera=useCamera)
@@ -28,6 +31,11 @@ if __name__=="__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument('runName', type=str, default=None)
+    parser.add_argument('--camera', dest="camera", action='store_true', default=False)
     args, extras = parser.parse_known_args()
 
-    runThem(args.runName)
+    runThem(args.runName, useCamera=args.camera)
+
+
+    # To run in parallel:
+    # cat runList.txt | xargs -P 3 -I CMD bash -c './genOrbitFiles.py CMD --camera'
